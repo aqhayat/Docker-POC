82b3d0f01a4845a6d236d423adc7015c74c12116
Shahid Hussain <shussain@nisum.com>
2019-03-05 16:43:15 +0500

    new services are added

---
+++ com-contract-service/src/test/java/com/contract/comcontractservice/ComContractServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.contract.comcontractservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComContractServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
---
+++ com-notification-service/src/main/java/com/notification/comnotificationservice/ComNotificationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.notification.comnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComNotificationServiceApplication.class, args);
+	}
+
+}
+
